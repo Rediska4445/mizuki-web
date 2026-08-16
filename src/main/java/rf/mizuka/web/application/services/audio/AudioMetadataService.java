cc932bf06b2989c7b22a2b4595519faf82d6b7d5
2022 <ivan.parysov@mail.ru>
2026-08-16 14:21:54 +0300

    refactor: AudioMetadataService

--- src/main/java/rf/mizuka/web/application/services/audio/AudioMetadataService.java
+++ src/main/java/rf/mizuka/web/application/services/audio/AudioMetadataService.java
@@ -2,51 +2,75 @@ package rf.mizuka.web.application.services.audio;
 
 import org.jaudiotagger.audio.AudioFile;
 import org.jaudiotagger.audio.AudioFileIO;
+import org.jaudiotagger.audio.exceptions.CannotReadException;
+import org.jaudiotagger.audio.exceptions.InvalidAudioFrameException;
+import org.jaudiotagger.audio.exceptions.ReadOnlyFileException;
 import org.jaudiotagger.tag.FieldKey;
 import org.jaudiotagger.tag.Tag;
+import org.jaudiotagger.tag.TagException;
 import org.jaudiotagger.tag.images.Artwork;
 import org.springframework.stereotype.Service;
-import rf.mizuka.web.application.models.tracks.Track;
+import rf.mizuka.web.application.database.entities.media.tracks.Track;
 
 import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 @Service
 public final class AudioMetadataService
 {
-    public Track extractMetadata(Track res)
-            throws Exception
+    public record Metadata(String title, Set<String> authors, Duration Duration, byte[] rawImage) {}
+
+    public String convertDurationToString(int totalSeconds)
     {
-        File file = new File(res.getFilePath());
+        return totalSeconds < 3600 ?
+                String.format("%d:%d", (totalSeconds % 3600) / 60, totalSeconds % 60)
+                : String.format("%d%d:%d", totalSeconds / 3600, (totalSeconds % 3600) / 60, totalSeconds % 60);
+    }
+
+    public Metadata extractMetadata(final Track tr)
+            throws IOException,
+                CannotReadException, TagException, InvalidAudioFrameException, ReadOnlyFileException,
+                UnknownTitleException, UnknownAuthorException
+    {
+        File file = new File(tr.getFilePath());
         AudioFile audioFile = AudioFileIO.read(file);
         Tag tag = audioFile.getTag();
 
-        res.setTitle(tag.getFirst(FieldKey.TITLE));
-        if (res.getTitle() == null || res.getTitle().isEmpty()) {
+        String title = tag.getFirst(FieldKey.TITLE);
+        if (title == null || title.isEmpty())
+        {
             throw new UnknownTitleException("Unknown title");
         }
 
-        res.setAuthor(tag.getFirst(FieldKey.ARTIST));
-        if (res.getAuthor() == null || res.getAuthor().isEmpty()) {
-            throw new UnknownAuthorException("Unknown author");
+        String authors = tag.getFirst(FieldKey.ARTIST);
+        if (authors == null || authors.isEmpty())
+        {
+            throw new UnknownAuthorException("Unknown authors");
         }
 
-        res.setDuration((short) audioFile.getAudioHeader().getTrackLength());
+        int duration = audioFile.getAudioHeader().getTrackLength();
+        if (duration <= 0)
+        {
+            throw new InvalidAudioDurationException("Duration must be positive");
+        }
+
+        byte[] rawImageData = null;
 
         Artwork artwork = tag.getFirstArtwork();
-
-        if (artwork != null) {
-            byte[] rawImageData = artwork.getBinaryData();
-            res.setPicture(rawImageData);
+        if (artwork != null)
+        {
+            rawImageData = artwork.getBinaryData();
         }
 
-        return res;
-    }
-
-    public Track extractMetadata(String filePath)
-            throws Exception
-    {
-        Track tr = new Track();
-        tr.setFilePath(filePath);
-        return extractMetadata(tr);
+        return new Metadata(
+                title,
+                new HashSet<>(Arrays.stream(authors.split(",")).toList()),
+                Duration.ofSeconds(duration),
+                rawImageData
+        );
     }
 }
