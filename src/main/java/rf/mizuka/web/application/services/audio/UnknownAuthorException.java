3a027df9e4e574329d051bff20cacf392262898e
2022 <ivan.parysov@mail.ru>
2026-08-16 14:23:09 +0300

    refactor: audio exceptions

--- src/main/java/rf/mizuka/web/application/services/audio/UnknownAuthorException.java
+++ src/main/java/rf/mizuka/web/application/services/audio/UnknownAuthorException.java
@@ -3,22 +3,26 @@ package rf.mizuka.web.application.services.audio;
 public class UnknownAuthorException
         extends Exception
 {
-    public UnknownAuthorException() {
-    }
+    public UnknownAuthorException()
+    {}
 
-    public UnknownAuthorException(Throwable cause) {
+    public UnknownAuthorException(Throwable cause)
+    {
         super(cause);
     }
 
-    public UnknownAuthorException(String message) {
+    public UnknownAuthorException(String message)
+    {
         super(message);
     }
 
-    public UnknownAuthorException(String message, Throwable cause) {
+    public UnknownAuthorException(String message, Throwable cause)
+    {
         super(message, cause);
     }
 
-    public UnknownAuthorException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+    public UnknownAuthorException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace)
+    {
         super(message, cause, enableSuppression, writableStackTrace);
     }
 }
--- src/main/java/rf/mizuka/web/application/services/audio/UnknownTitleException.java
+++ src/main/java/rf/mizuka/web/application/services/audio/UnknownTitleException.java
@@ -3,23 +3,26 @@ package rf.mizuka.web.application.services.audio;
 public class UnknownTitleException
         extends Exception
 {
-    public UnknownTitleException() {
+    public UnknownTitleException()
+    {}
 
-    }
-
-    public UnknownTitleException(Throwable cause) {
+    public UnknownTitleException(Throwable cause)
+    {
         super(cause);
     }
 
-    public UnknownTitleException(String message) {
+    public UnknownTitleException(String message)
+    {
         super(message);
     }
 
-    public UnknownTitleException(String message, Throwable cause) {
+    public UnknownTitleException(String message, Throwable cause)
+    {
         super(message, cause);
     }
 
-    public UnknownTitleException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+    public UnknownTitleException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace)
+    {
         super(message, cause, enableSuppression, writableStackTrace);
     }
 }
