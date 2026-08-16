c16a61c808f4e2680cb5b3281e7f0aa6cbb2e2b2
2022 <ivan.parysov@mail.ru>
2026-08-16 14:05:30 +0300

    edit: HomeController unlimited list tracks

--- src/main/java/rf/mizuka/web/application/controllers/home/HomeController.java
+++ src/main/java/rf/mizuka/web/application/controllers/home/HomeController.java
@@ -1,15 +1,12 @@
 package rf.mizuka.web.application.controllers.home;
 
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import rf.mizuka.web.application.models.tracks.Track;
+import rf.mizuka.web.application.forms.home.TrackForm;
 import rf.mizuka.web.application.services.tracks.TrackService;
 
 import java.security.Principal;
@@ -20,30 +17,29 @@ public final class HomeController
 {
     private final TrackService trackService;
 
-    public HomeController(TrackService trackService) {
+    public HomeController(TrackService trackService)
+    {
         this.trackService = trackService;
     }
 
     @GetMapping
     public String tracks(
-            @RequestParam(required = false) String search,
-            @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(required = false, defaultValue = "") String query,
+            @RequestParam(required = false, defaultValue = "0") int size,
             Model model,
             Principal principal
     ) {
-        Pageable pageable = PageRequest.of(page, size, Sort.by("name").descending());
-        Page<Track> trackPage = trackService.searchTracks(search, pageable);
+        Page<TrackForm> tracks = trackService.searchTracks(query, size).map(
+                e -> new TrackForm(
+                        e,
+                        trackService.encodeBase64Picture(e),
+                        trackService.audioService().audioMetadataService().convertDurationToString((int) e.getDuration().getSeconds())
+                ));
 
         String username = principal.getName().equals("default") ? "default" : principal.getName();
 
-        model.addAttribute("tracks", trackPage.getContent());
         model.addAttribute("username", username);
-        model.addAttribute("searchQuery", search);
-        model.addAttribute("currentPage", trackPage.getNumber());
-        model.addAttribute("totalPages", trackPage.getTotalPages());
-        model.addAttribute("totalElements", trackPage.getTotalElements());
-        model.addAttribute("pageSize", size);
+        model.addAttribute("tracks", tracks.getContent());
 
         return "home";
     }
