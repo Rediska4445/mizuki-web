c3728e456a07c945dcaed60af4d5105fd68b5116
2022 <ivan.parysov@mail.ru>
2026-08-31 09:05:41 +0300

    serializable marked in Track.java

--- src/main/java/rf/mizuka/web/application/database/entities/media/tracks/Track.java
+++ src/main/java/rf/mizuka/web/application/database/entities/media/tracks/Track.java
@@ -14,7 +14,12 @@ import java.util.Set;
 @AllArgsConstructor
 @Table(name = "tracks")
 public class Track
+        implements Serializable, Comparable<Track>
 {
+    @Serial
+    private static final long serialVersionUID
+            = 1L;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
