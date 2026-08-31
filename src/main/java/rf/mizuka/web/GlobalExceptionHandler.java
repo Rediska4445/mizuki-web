f1c3809fc387c872d59a24a6e2f2024bfe0693c6
2022 <ivan.parysov@mail.ru>
2026-08-31 08:35:51 +0300

    add: logging to GlobalExceptionHandler

--- src/main/java/rf/mizuka/web/GlobalExceptionHandler.java
+++ src/main/java/rf/mizuka/web/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package rf.mizuka.web;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.Map;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler
 {
