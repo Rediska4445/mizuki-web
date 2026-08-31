77163ab5a6e0b61acdf2c94dc26a50a75d019942
2022 <ivan.parysov@mail.ru>
2026-08-31 09:03:05 +0300

    add: enable kafka and cache in application

--- src/main/java/rf/mizuka/Application.java
+++ src/main/java/rf/mizuka/Application.java
@@ -2,6 +2,8 @@ package rf.mizuka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
 public class Application {
