f19284a187847f67ced0fc327acfcc393216ded8
2022 <ivan.parysov@mail.ru>
2026-09-05 15:19:45 +0300

    add: fileOwner, isExplicit, likesCount, playsCount, description, authors to Track entity

--- src/main/java/rf/mizuka/web/application/database/entities/media/tracks/Track.java
+++ src/main/java/rf/mizuka/web/application/database/entities/media/tracks/Track.java
@@ -1,16 +1,26 @@
 package rf.mizuka.web.application.database.entities.media.tracks;
 
+import com.google.common.base.Objects;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.validator.constraints.time.DurationMin;
+import org.jetbrains.annotations.NotNull;
 import rf.mizuka.web.application.database.entities.media.authors.Author;
+import rf.mizuka.web.application.database.entities.user.User;
 
-import java.awt.*;
+import java.io.Serial;
+import java.io.Serializable;
+import java.sql.Types;
 import java.time.Duration;
 import java.util.Set;
 
 @Data
 @Entity
+@Builder
 @AllArgsConstructor
 @Table(name = "tracks")
 public class Track
@@ -26,18 +36,56 @@ public class Track
 
     /* Metadata */
     @Column(length = 256)
+    @Basic(fetch = FetchType.LAZY)
     private String picturePath;
 
     @Column(length = 7)
+    @Basic(fetch = FetchType.LAZY)
     private String color;
 
     @Column(length = 128)
+    @Basic(fetch = FetchType.LAZY)
     private String title;
 
-    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User fileOwner;
+
+    @Column
+    @Basic(fetch = FetchType.LAZY)
+    private Boolean isExplicit
+            = Boolean.FALSE;
+
+    @Column(nullable = false)
+    @Min(value = 0)
+    private Long likesCount
+            = 0L;
+
+    @Column(nullable = false)
+    @Min(value = 0)
+    private Long playsCount
+            = 0L;
+
+    @Column(columnDefinition = "TEXT")
+    @Basic(fetch = FetchType.LAZY)
+    private String description;
+
+    @JoinTable(
+            name = "tracks_authors",
+            joinColumns = @JoinColumn(name = "tracks_id"),
+            inverseJoinColumns = @JoinColumn(name = "authors_id")
+    )
+    @ManyToMany(cascade =
+    {
+            CascadeType.PERSIST,
+            CascadeType.MERGE
+    })
     private Set<Author> authors;
 
     @Column
+    @DurationMin(seconds = 0)
+    @Basic(fetch = FetchType.LAZY)
+    @JdbcTypeCode(Types.BIGINT)
     private Duration duration;
 
     /* Physical data */
