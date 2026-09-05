d2f4a35851bc8f046907fe2916e5c23846ead48e
2022 <ivan.parysov@mail.ru>
2026-09-05 15:11:15 +0300

    SecurityConfig comment for security ruleset

--- src/main/java/rf/mizuka/web/application/configurations/security/SecurityConfig.java
+++ src/main/java/rf/mizuka/web/application/configurations/security/SecurityConfig.java
@@ -1,6 +1,5 @@
-package rf.mizuka.web.application.security.config;
+package rf.mizuka.web.application.configurations.security;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,20 +21,36 @@ import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import rf.mizuka.web.application.database.repository.UserRepository;
+import rf.mizuka.web.application.database.repository.user.UserRepository;
 import rf.mizuka.web.application.services.user.CustomUserDetailsService;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig
 {
-    @Autowired // <-- Legacy code
-    private UserRepository userRepository;
-    @Autowired // <-- Legacy code
-    @Qualifier("bCryptPasswordEncoder")
-    private PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
 
-    /* Main filter chain for usually users */
+    @Qualifier("bCryptPasswordEncoder")
+    private final PasswordEncoder passwordEncoder;
+
+    public SecurityConfig(UserRepository userRepository, PasswordEncoder passwordEncoder)
+    {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    /*
+        Main filter chain for usually users.
+        Rules:
+        1. Unauthorized users cannot access any service pages, except for the login page.
+        2. Authorized users can access the following pages: home, tracks.
+        3. All users have access to specific static assets (js, css, html, ico, img).
+        4. All operations must be performed using CSRF token technology.
+        5. API access is prohibited for everyone.
+        6. Authorized users can terminate their session (logout).
+        ---
+        All necessary tests must adhere to and verify the specified rules.
+    */
     @Bean
     @Order(2)
     public SecurityFilterChain webSecurityFilterChain(HttpSecurity http)
@@ -48,9 +63,11 @@ public class SecurityConfig
                 )
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/**").denyAll()
+                        .requestMatchers("/.well-known/**").denyAll()
                         .requestMatchers("/favicon.ico", "/css/**", "/js/**", "/icons/**").permitAll()
-                        .requestMatchers("/auth/login", "/auth/register").permitAll().
-                        requestMatchers("/track/**").authenticated()
+                        .requestMatchers("/auth/login", "/auth/register").permitAll()
+                        .requestMatchers("/track/**", "/tracks/**").authenticated()
+                        .requestMatchers("/audio/**").authenticated()
                         .anyRequest().authenticated()
                 )
                 .exceptionHandling(eh -> eh
@@ -105,10 +122,14 @@ public class SecurityConfig
         LoginUrlAuthenticationEntryPoint entryPoint =
                 new LoginUrlAuthenticationEntryPoint("/auth/login");
 
-        return (request, response, accessDeniedException) -> {
-            if (request.getServletPath().startsWith("/api/")) {
+        return (request, response, accessDeniedException) ->
+        {
+            if (request.getServletPath().startsWith("/api/"))
+            {
                 response.setStatus(HttpStatus.FORBIDDEN.value());
-            } else {
+            }
+            else
+            {
                 entryPoint.commence(request, response, null);
             }
         };
