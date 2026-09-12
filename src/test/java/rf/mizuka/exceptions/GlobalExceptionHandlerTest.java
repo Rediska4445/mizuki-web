9d95cb8e3e84aedd2bc2bf9ea6a7c70f3f54d53a
2022 <ivan.parysov@mail.ru>
2026-09-12 11:08:24 +0300

    add: bad credientials test

--- src/test/java/rf/mizuka/exceptions/GlobalExceptionHandlerTest.java
+++ src/test/java/rf/mizuka/exceptions/GlobalExceptionHandlerTest.java
@@ -14,8 +14,10 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import rf.mizuka.web.GlobalExceptionHandler;
 import rf.mizuka.web.application.configurations.security.SecurityConfig;
 import rf.mizuka.web.application.database.repository.user.UserRepository;
 
@@ -49,6 +51,11 @@ public class GlobalExceptionHandlerTest
         CacheManager realCacheManager = new ConcurrentMapCacheManager();
         Mockito.when(cacheManager.getCache(Mockito.anyString()))
                 .thenAnswer(invocation -> realCacheManager.getCache(invocation.getArgument(0)));
+
+        this.mockMvc = MockMvcBuilders
+                .standaloneSetup(new FakeController())
+                .setControllerAdvice(new GlobalExceptionHandler())
+                .build();
     }
 
     @RestController
@@ -79,5 +86,18 @@ public class GlobalExceptionHandlerTest
                 .andExpect(status().is5xxServerError())
                 .andExpect(jsonPath("$.error").value("The server was rude to me :("))
                 .andExpect(jsonPath("$.message").value("ask the administrator to make the server apologize!"));
+    }
+
+    /*
+     * 1. Any bad credentials exceptions should return '4xx' error code and JSON answer for error subscription
+     * */
+    @Test
+    @DisplayName("Обработчик исключений должен возвращать статус 500 и кастомный JSON при любой ошибке")
+    void shouldBadCredentialsHandleAnyExceptionAndReturnCustomJson()
+            throws Exception
+    {
+        mockMvc.perform(get("/auth/login/test-4x-exception"))
+                .andExpect(status().is4xxClientError())
+                .andExpect(jsonPath("$.error").value("Entity is unauthorized"));
     }
 }
